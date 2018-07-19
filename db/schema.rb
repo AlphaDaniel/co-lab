2f0018d39e8b82de06b5e696da250144a1c4999a
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-07-19 05:25:45 -0400

    added projects model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_085208) do
+ActiveRecord::Schema.define(version: 2018_07_19_092134) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "lang"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
