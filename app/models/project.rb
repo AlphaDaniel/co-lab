e2c06a6d345781540de4061c0c6da9ccddb6101c
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-07-19 06:18:11 -0400

    set validations for user and projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
+  
+  validates :name, :lang, :description, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 10 }
 end
