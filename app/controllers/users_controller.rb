cd6d7a5ec1662f2b74f4a7b72242558f5792eaa9
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-07-22 17:07:38 -0400

    created user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  
   def index
     if params[:project_id]
       @project = Project.find_by(id: params[:project_id])
@@ -7,5 +9,11 @@ class UsersController < ApplicationController
       
       return redirect_to project_path(@project) if @project.nil?
     end
+  end
+  
+private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
