4a9a7a2e23455cd7555f45b69464f9003db49a74
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-07-22 09:43:21 -0400

    plugged up new project flow + minimal styling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,23 @@
 class ProjectsController < ApplicationController
+  helper_method [:project_errors]
   before_action :log_in_required
   before_action :set_project, only: [:show]
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(project_params)
+    
+    if @project.save
+      up = UserProject.new(user_id: current_user.id, project_id: @project.id, role: 'Creator')
+      up.save
+      redirect_to @project
+    else
+      render :new
+    end
+  end
   
   def show
   end
@@ -9,5 +26,17 @@ private
 
   def set_project
     @project = Project.find_by(id: params[:id])
+  end
+  
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :lang,
+      :description,
+    )
+  end
+  
+  def project_errors
+    @project.errors.full_messages
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,17 @@ class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
   
-  validates :name, :lang, :description, presence: true
+  validates :name, :description, presence: true
+  validates :lang, presence: true
   validates :name, uniqueness: true
   validates :description, length: { minimum: 10 }
+  
+  HUMANIZED_ATTRIBUTES = {
+  :lang => "Language"
+  }
+  def self.human_attribute_name(attr, options={})
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
   
   def self.all_desc
     order(created_at: :desc)
