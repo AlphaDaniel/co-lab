92caa238b1aa7cee87917140ea77de888fa19b91
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-07-24 19:58:10 -0400

    configured permitted params (:name) for devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method [:all_projects, :log_in_required]
+  
+private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   
   def log_in_required
     redirect_to new_user_session_path, alert: "Must be logged in" unless user_signed_in?
